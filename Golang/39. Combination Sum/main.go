d0a251e75209e2f83e7abf502f1e205e2a071b2e
withlzc <withlzc@gmail.com>
2020-03-28 23:48:18 +0800

    update 39

--- Golang/39. Combination Sum/main.go
+++ Golang/39. Combination Sum/main.go
@@ -5,29 +5,30 @@ import (
 	"sort"
 )
 
-var ans [][]int
-
 func combinationSum(candidates []int, target int) [][]int {
-	ans = nil
+	var ans [][]int
 	var temp []int
+	if len(candidates) == 0 {
+		return [][]int{}
+	}
 	sort.Ints(candidates)
-	backtrack(temp, candidates, target, 0)
+	backtrack(&ans, temp, candidates, target, 0)
 	return ans
 }
 
-func backtrack(temp []int, candidates []int, remain, start int) {
-	if remain < 0 {
-	} else if remain == 0 {
-		ans = append(ans, append([]int{}, temp...))
+func backtrack(ans *[][]int, temp []int, candidates []int, remain, start int) {
+	if remain == 0 {
+		*ans = append(*ans, append([]int{}, temp...))
+	} else if remain < candidates[0] {
 	} else {
 		for i := start; i < len(candidates); i++ {
-			backtrack(append(temp, candidates[i]), candidates, remain-candidates[i], i)
+			backtrack(ans, append(temp, candidates[i]), candidates, remain-candidates[i], i)
 		}
 	}
 }
 
 func main() {
-	candidates := []int{2, 3, 5}
+	candidates := []int{}
 	target := 8
 	ans := combinationSum(candidates, target)
 	fmt.Println(ans)
