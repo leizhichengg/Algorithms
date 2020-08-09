f10ff745fca3a8bf6b81732e85686608bf3bebbf
withlzc <withlzc@gmail.com>
2020-08-09 11:06:33 +0800

    update 2 problems

--- Golang/198. House Robber/main.go
+++ Golang/198. House Robber/main.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func rob(nums []int) int {
-	pre1, pre2 := 0, 0
-	for i := 0; i < len(nums); i++ {
-		cur := max(pre1, pre2+nums[i])
-		pre2 = pre1
-		pre1 = cur
-	}
-	return pre1
-}
+//func rob(nums []int) int {
+//	pre1, pre2 := 0, 0
+//	for i := 0; i < len(nums); i++ {
+//		cur := max(pre1, pre2+nums[i])
+//		pre2 = pre1
+//		pre1 = cur
+//	}
+//	return pre1
+//}
 
 func max(m, n int) int {
 	if m >= n {
@@ -18,6 +18,24 @@ func max(m, n int) int {
 	} else {
 		return n
 	}
+}
+
+//DP
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	//dp[i]表示偷窃前i家的最高金额
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+	}
+	return dp[len(nums)-1]
 }
 
 func main() {
--- Golang/5. Longest Palindromic Substring/main.go
+++ Golang/5. Longest Palindromic Substring/main.go
@@ -37,27 +37,27 @@ import "fmt"
 //	return res
 //}
 
-func longestPalindrome(s string) string {
-	length := len(s)
-	if length <= 1 {
-		return s
-	}
-	res := string(s[0])
-	maxLen := 0
-	for i := 0; i < length; i++ {
-		len1, str1 := isPalindromeByCenter(s, i, i)
-		len2, str2 := isPalindromeByCenter(s, i, i+1)
-		if len1 > len2 && len1 > maxLen {
-			maxLen = len1
-			res = str1
-		}
-		if len2 > len1 && len2 > maxLen {
-			maxLen = len2
-			res = str2
-		}
-	}
-	return res
-}
+//func longestPalindrome(s string) string {
+//	length := len(s)
+//	if length <= 1 {
+//		return s
+//	}
+//	res := string(s[0])
+//	maxLen := 0
+//	for i := 0; i < length; i++ {
+//		len1, str1 := isPalindromeByCenter(s, i, i)
+//		len2, str2 := isPalindromeByCenter(s, i, i+1)
+//		if len1 > len2 && len1 > maxLen {
+//			maxLen = len1
+//			res = str1
+//		}
+//		if len2 > len1 && len2 > maxLen {
+//			maxLen = len2
+//			res = str2
+//		}
+//	}
+//	return res
+//}
 
 func isPalindromeByCenter(s string, left, right int) (int, string) {
 	l, r := left, right
@@ -75,7 +75,41 @@ func max(m, n int) int {
 	return n
 }
 
+//DP
+func longestPalindrome(s string) string {
+	length := len(s)
+	if length <= 1 {
+		return s
+	}
+	maxLen := 1
+	res := s[0:1]
+	//dp[i][j]表示从s[i]到s[j]是否构成回文字符串
+	dp := make([][]bool, length)
+	for i := 0; i < length; i++ {
+		dp[i] = make([]bool, length)
+	}
+	for i := 0; i < length; i++ {
+		dp[i][i] = true
+	}
+	for j := 1; j < length; j++ {
+		for i := 0; i < j; i++ {
+			if s[i] == s[j] {
+				if j-i <= 2 {
+					dp[i][j] = true
+				} else {
+					dp[i][j] = dp[i+1][j-1]
+				}
+			}
+			if dp[i][j] && j-i+1 > maxLen {
+				maxLen = j - i + 1
+				res = s[i : j+1]
+			}
+		}
+	}
+	return res
+}
+
 func main() {
-	ans := longestPalindrome("abacab")
+	ans := longestPalindrome("ab")
 	fmt.Println(ans)
 }
